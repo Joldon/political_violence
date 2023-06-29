b052d9e140bbeb234487bf2872c1d79e7112667b
joldon <79482239+Joldon@users.noreply.github.com>
2023-06-29 11:30:16 +0200

    code clean-up

--- dashboards/src/components/EmbedTableau.js
+++ dashboards/src/components/EmbedTableau.js
@@ -1,11 +1,17 @@
 import React, {useRef, useEffect} from 'react'
 // import tableau from 'react-tableau'
+// import {
+//   TableauViz,
+// } from 'https://public.tableau.com/javascripts/api/tableau.embedding.3.latest.min.js';
 
 const { tableau} = window
 
 function EmbedTableau() {
     const ref = useRef(null)
+    // below is the url link to the tableau server that is not currently available
     // const url = 'https://eu-west-1a.online.tableau.com/#/site/politicalviolence/views/Syriawithdiscretefilter/Syria?:iid=1'
+
+    // this is working url connected to the tableau public server
     const url = 'https://public.tableau.com/views/Democracy_postcommunist_scores/Democracyinpostcommunistcountries2005-2022'
 
     function initViz() {
@@ -18,22 +24,13 @@ function EmbedTableau() {
     useEffect(() => {
         initViz()
     }, [])
-    // useEffect(() => {
-    //     const initViz = () => {
-    //       if (ref.current) {
-    //         new tableau.Viz(ref.current, url);
-    //       }
-    //     };
-    
-    //     initViz();
-    //   }, []);
   return (
     <div>
         <h1>EmbedTableau</h1>
         <div ref={ref}></div>
-        {/* <tableau-viz id="tableauViz"       
-        src='https://eu-west-1a.online.tableau.com/#/site/politicalviolence/views/Syriawithdiscretefilter/Syria?:iid=1'>
-        </tableau-viz> */}
+        <tableau-viz id="tableauViz"       
+        src={url}>
+        </tableau-viz>
     </div>
   )
 }
